b264d06e43a08b4c338e01f504c7fffdf1679d80
Alexandre Freire Pellegrino <alexandre.pellegrino@simlabsdev.onmicrosoft.com>
2022-09-07 16:12:42 -0300

    feat: boilerplate

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: true
 }
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
+
+module.exports = withPWA(nextConfig)
